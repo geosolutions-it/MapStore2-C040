1aa1dab5e1d62b671b8dc793a8d0771066e17df3
Vladimir <illarifin@gmail.com>
2020-06-19 20:55:53 +0300

    return comge config, update package

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ const config = require('./MapStore2/build/buildConfig')(
         "llpp": path.join(__dirname, "js", "llpp")
     },
     {
-        "themes/default": path.join(__dirname, "assets", "themes", "comge", "theme.less")
+        "themes/default": path.join(__dirname, "assets", "themes", "comge", "theme.less"),
+        "themes/comge": path.join(__dirname, "assets", "themes", "comge", "theme.less")
     },
     {
         base: __dirname,
